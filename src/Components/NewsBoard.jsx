632f198d64b7bee2a018b8ba88b1a36bdfa31052
Nikhil <nikhilbchandran@gmail.com>
2024-03-12 17:49:30 +0530

    last but not full

--- src/Components/NewsBoard.jsx
+++ src/Components/NewsBoard.jsx
@@ -1,22 +1,22 @@
-import { useEffect, useState } from "react"
+import { useState } from "react";
+import { useEffect } from "react";
+
 import NewsItem from "./NewsItem";
 
 
-const NewsBoard = () => {
+const NewsBoard = ({category}) => {
 
-    const [articles,setArticles] = useState([]);
+    const [articles,setArticles] = useState([])
 
     useEffect(() => {
-        const api_key = import.meta.env.VITE_API_KEY;
-        console.log("api_key:", api_key); 
       
-        let url = `https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=bb2d5e6802db4ae481017d443dc3879e`;
+        let url = `https://newsapi.org/v2/top-headlines?country=in&category=${category}&apiKey=bb2d5e6802db4ae481017d443dc3879e`;
         console.log(url, "urrllll");
         fetch(url)
-          .then(response => console.log(response))
+          .then(response => response.json())
           .then(data => setArticles(data.articles))
           .catch(error => console.error('Error fetching data:', error));
-      }, []);
+      }, [category]);
       
 
 
